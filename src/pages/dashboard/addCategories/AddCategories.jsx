e7cee9489ce4984035c025dd7fedd4ae3debfd4a
almas hossain <almashossain7384@gmail.com>
2025-02-11 15:20:19 +0600

    add categorie section design ok

--- src/pages/dashboard/addCategories/AddCategories.jsx
+++ src/pages/dashboard/addCategories/AddCategories.jsx
@@ -1,5 +1,5 @@
 
-import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined, UploadOutlined } from "@ant-design/icons";
 import { Button, Form, Input, Table, Modal } from "antd";
 import Upload from "antd/es/upload/Upload";
 import { useState } from "react";
@@ -7,13 +7,37 @@ import { useState } from "react";
 const AddCategories = () => {
   const [form] = Form.useForm();
   const [dataSource, setDataSource] = useState([
-    { id: 1, name: "John", address: "Dhaka" },
-    { id: 2, name: "Kamal", address: "Fulbaria" },
-    { id: 3, name: "Monir", address: "Asim" },
+    { id: 1, name: "John", image: "/legalImage/legal1.png" },
+    { id: 2, name: "Kamal", image: "/legalImage/legal2.png" },
+    { id: 3, name: "Monir", image: "/legalImage/legal3.png" },
+    { id: 4, name: "John", image: "/legalImage/legal1.png" },
+    { id: 5, name: "Kamal", image: "/legalImage/legal2.png" },
+    { id: 6, name: "Monir", image: "/legalImage/legal3.png" },
   ]);
+
+  const columns = [
+    { key: "1", title: "Image", dataIndex: "image", responsive: ["xs", "sm", "md", "lg", "xl"], render: (image) => <img src={image} alt="User" className="w-12 h-12 object-cover rounded-md" /> },
+
+    { key: "2", title: "Name", dataIndex: "name", responsive: ["xs", "sm", "md", "lg", "xl"] },
+
+    {
+      key: "4",
+      title: "Action",
+      responsive: ["xs", "sm", "md", "lg", "xl"], // Mobile & Tablet Responsive
+      render: (record) => (
+        <div className="flex gap-2 items-center">
+          {/* <EditOutlined className="text-blue-500 cursor-pointer text-lg" /> */}
+          <Button type="primary" danger onClick={() => showDeleteModal(record)}>
+            <DeleteOutlined />
+          </Button>
+        </div>
+      ),
+    },
+  ];
 
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedRecord, setSelectedRecord] = useState(null);
+  const [fileList, setFileList] = useState(null)
 
   const showDeleteModal = (record) => {
     setSelectedRecord(record);
@@ -29,24 +53,47 @@ const AddCategories = () => {
     setIsModalVisible(false);
   };
 
-  const columns = [
-    { key: "1", title: "Image", dataIndex: "image", responsive: ["xs", "sm", "md", "lg", "xl"] },
-    { key: "2", title: "Name", dataIndex: "name", responsive: ["xs", "sm", "md", "lg", "xl"] },
 
-    {
-      key: "4",
-      title: "Action",
-      responsive: ["xs", "sm", "md", "lg", "xl"], // Mobile & Tablet Responsive
-      render: (record) => (
-        <div className="flex gap-2 items-center">
-          {/* <EditOutlined className="text-blue-500 cursor-pointer text-lg" /> */}
-          <Button type="primary" danger  onClick={() => showDeleteModal(record)}>
-          <DeleteOutlined />
-          </Button> 
-        </div>
-      ),
-    },
-  ];
+  const handleUpload = (info) => {
+    setFileList([...info.fileList || []]);
+  }
+
+
+  const handleCategorie = async (values) => {
+    const formData = new FormData();
+
+    // Append image file
+    if (fileList && fileList.length > 0) {
+      formData.append("image", fileList[0].originFileObj);
+    }
+
+    formData.append("name", values.name);
+
+
+
+
+    
+    formData.forEach((value, key) => {
+      console.log(`${key}:`, value);
+    })
+
+    // try {
+    //   const response = await axios.post("url", formData, {
+    //     headers: {
+    //       "Content-Type": "multipart/form-data",
+    //     },
+    //   });
+
+    //   console.log("Success:", response.data);
+
+    //   form.resetFields();
+    //   setFileList([]);
+
+    // } catch (error) {
+    //   console.error("Error:", error.response ? error.response.data : error.message);
+    // }
+  };
+
 
   return (
     <div className="bg-white p-4 rounded-lg max-w-full">
@@ -56,39 +103,42 @@ const AddCategories = () => {
       </div>
 
       {/* Form */}
-      <div className="">
-        <Form form={form}>
+      <div className="pt-3">
+        <Form form={form} onFinish={handleCategorie}>
 
-          {/* Image Upload Field */}
-          <div className=" mb-2 w-full">
+          {/* upload image */}
+          <div>
             <p className="font-roboto text-[#41414D] text-[14px]">Upload</p>
             <Form.Item
-            
-              name="image"
+              name="upload"
               valuePropName="fileList"
-              getValueFromEvent={(e) => (Array.isArray(e) ? e : e?.fileList)}
+              getValueFromEvent={(e) => e?.fileList || []} 
               rules={[
-                { required: true, message: "Please upload an image!" },
-                { validator: (_, value) => (value?.[0]?.type?.startsWith("image/") ? Promise.resolve() : Promise.reject("File must be an image")) },
+                {
+                  required: true,
+                  message: "Please upload an image!",
+                },
               ]}
             >
-              <Upload beforeUpload={() => false} listType="picture">
-                <Button style={{ width: '1050px',padding:"20px", }}>Upload Image</Button>
+              <Upload beforeUpload={() => false} onChange={handleUpload} fileList={fileList}>
+                <Button size="large" icon={<UploadOutlined />}>Upload Categorie Image</Button>
               </Upload>
             </Form.Item>
           </div>
 
-          <p className="font-roboto text-[#41414D] text-[14px]">Categories name</p>
-          <Form.Item
-            name="name"
-            rules={[{ required: true, message: "Please enter your category name" }]}
-          >
-            <Input
-              type="text"
-              placeholder="Enter title"
-              className="border border-[#b6b6ba83] px-3 py-2 w-full"
-            />
-          </Form.Item>
+          <div>
+            <p className="font-roboto text-[#41414D] text-[14px]">Categories name</p>
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: "Please enter your category name" }]}
+            >
+              <Input
+                type="text"
+                placeholder="Enter title"
+                className="border border-[#b6b6ba83] px-3 py-2 w-full"
+              />
+            </Form.Item>
+          </div>
 
           <Button
             htmlType="submit"
@@ -109,7 +159,7 @@ const AddCategories = () => {
         <Table
           columns={columns}
           dataSource={dataSource}
-          pagination={{ pageSize: 5 }}
+          pagination={{ pageSize: 4 }}
           className="w-full"
         />
       </div>
