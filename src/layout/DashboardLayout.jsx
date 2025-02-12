6801bd81539baad15e6e591da9d67017fb0d2e3d
almas hossain <almashossain7384@gmail.com>
2025-02-12 11:34:48 +0600

    sidebar route icon add

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -10,21 +10,22 @@ const DashboardLayout = () => {
     return (
         <div className="relative flex h-screen">
             {/* Sidebar component */}
-            <div className="hidden lg:block w-1/5 bg-[#FFFF] h-full fixed">
+            <div className="hidden lg:block w-[270px] bg-gray-200 h-full fixed border-2 border-red-600">
                 <Sidebar />
             </div>
 
+
             {/* ============= mobile & tablet sidebar start ========== */}
             {
-                showDrawer && <div className="lg:hidden absolute w-[86%] md:w-[40%] h-screen bg-red-200 z-50">
+                showDrawer && <div className="lg:hidden absolute w-[86%] md:w-[40%] h-screen bg-[#FFFF] z-50">
                     <div className="flex items-center justify-between px-3 py-[18px]">
                         <div className="flex items-center gap-2">
                             <img src="/logo.png" alt="logo" />
                             {/* <h1 className="text-xl font-semibold">AdminLayout</h1> */}
                         </div>
                         <FaArrowLeft
-                        onClick={()=>setShowDrawer(!showDrawer)}
-                        className="text-2xl"/>
+                            onClick={() => setShowDrawer(!showDrawer)}
+                            className="text-2xl" />
                     </div>
                 </div>
             }
@@ -32,15 +33,30 @@ const DashboardLayout = () => {
 
 
             {/* Main content area */}
-            <div className=" lg:ml-[20%] w-full lg:w-[80%] flex flex-col">
+            <div className="ml-[290px] w-full flex flex-col">
                 <div className="order-1">
                     <DashboardNavbar showDrawer={showDrawer} setShowDrawer={setShowDrawer} />
                 </div>
 
-                <div className="order-2 flex-1 overflow-y-auto p-6 bg-gray-300">
+                <div className="order-2 flex-1 overflow-y-auto p-6 bg-red-400 rounded-md">
                     <Outlet />
                 </div>
             </div>
+
+
+            {/* {
+                showDrawer && <div className="lg:hidden absolute w-[86%] md:w-[40%] h-screen bg-[#FFFF] z-50">
+                    <div className="flex items-center justify-between px-3 py-[18px]">
+                        <div className="flex items-center gap-2">
+                            <img src="/logo.png" alt="logo" />
+                        </div>
+                        <FaArrowLeft
+                            onClick={() => setShowDrawer(!showDrawer)}
+                            className="text-2xl" />
+                    </div>
+                </div>
+            } */}
+
         </div>
     );
 };
