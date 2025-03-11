975ee79e6af0ba6320b09ee32666339becdd21da
Almas Hossain <almashossain7384@gmail.com>
2025-03-11 12:21:48 +0600

    dashboard about section text edition component changes

--- src/pages/dashboard/settings/nestedRoute/DashboardAbout.jsx
+++ src/pages/dashboard/settings/nestedRoute/DashboardAbout.jsx
@@ -2,14 +2,17 @@
 import React, { useState, useRef } from 'react';
 import JoditEditor from 'jodit-react';
 import { Button } from 'antd';
+import Editor from '../../../../components/shared/editor/Editor';
 
 const DashboardAbout = () => {
   const [content, setContent] = useState('');
+  const [updateContent, setUpdateContent] = useState('');
+
   const editor = useRef(null); // Correctly initialize ref
 
 
   const handleUpdate = async () => {
-    console.log(content)
+    setUpdateContent(content)
     // try {
 
     //   const response = await axios.post('url', { content });
@@ -31,7 +34,7 @@ const DashboardAbout = () => {
         <p className="fontro text-[#B6B6BA] text-[12px] pb-3">Admin can edit personal information</p>
 
         <div className="w-full mt-6">
-          <JoditEditor
+          {/* <JoditEditor
             ref={editor}
             value={content}
             // config={{
@@ -41,7 +44,8 @@ const DashboardAbout = () => {
               console.log("Editor Content:", newContent);
               setContent(newContent);
             }}
-          />
+          /> */}
+          <Editor content={content} setContent={setContent}/>
         </div>
 
         <Button
@@ -57,7 +61,7 @@ const DashboardAbout = () => {
         <div className="mt-4">
           <h1 className="font-roboto text-[20px] md:text-[40px] font-bold text-[#10101E]">Preview update content:</h1>
           <p className="fontro text-[#B6B6BA] text-[12px] pb-3">Admin writes about us</p>
-          <div dangerouslySetInnerHTML={{ __html: content }} />
+          <div dangerouslySetInnerHTML={{ __html: updateContent }} />
         </div>
       </div>
     </div>
