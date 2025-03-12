3bbcaf8b6ff8fec7c51fdf6e4d21ad9beb182d62
almas hossain <almashossain7384@gmail.com>
2025-03-12 21:43:10 +0600

    lawyer profile create modal ok

--- src/pages/otpCode/OtpCode.jsx
+++ src/pages/otpCode/OtpCode.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Modal, Space, Select } from "antd";
+import { Form, Input, Button, Modal, Space, Select, Upload, TimePicker } from "antd";
 import { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import AccountCreate from "../../layout/AccountCreate";
@@ -6,6 +6,7 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Cookies from "js-cookie";
 import toast from "react-hot-toast";
 import Title from "antd/es/skeleton/Title";
+import { UploadOutlined } from "@ant-design/icons";
 
 
 const OtpCode = () => {
@@ -19,12 +20,30 @@ const OtpCode = () => {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [issModalOpenTwo, setIsModalOpenTwo] = useState(false);
+    const [issModalOpenThree, setIsModalOpenThree] = useState(false)
+
     const [categorieData, setCategorieData] = useState([]);
     const [selectedOptions, setSelectedOptions] = useState([]);
+
     const [modalOneValue, setModalOneValue] = useState([])
+    const [modalTwoValue, setModalTwoValue] = useState({
+        practice: "",
+        location: "",
+        languages: "",
+        address: "",
+        state: "",
+        zipCode: "",
+    });
 
 
-    const [modalTwoValue, setModalTwoValue] = useState()
+    // modal three
+    const [fileList, setFileList] = useState([]);
+    const [webLink, setWebLink] = useState("");
+    const [availability, setAvailability] = useState(null);
+    const [startTime, setStartTime] = useState(null);
+    const [endTime, setEndTime] = useState(null);
+
+
 
 
     const onFinish = async (values) => {
@@ -88,6 +107,8 @@ const OtpCode = () => {
         fetchData();
     }, []);
 
+    // Handle File Upload
+    const handleChange = ({ fileList }) => setFileList(fileList);
 
 
     //====================== first modal start ==============
@@ -126,21 +147,23 @@ const OtpCode = () => {
 
 
     //====================== second modal start =================
-    const handleSelectModalLowyerTwoValue = (key, value) => {
-        setModalTwoValue(prev => ({
-            ...prev,
-            [key]: value
-        }))
-    }
-
+    // Handle input change
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        handleSelectModalLowyerTwoValue(name, value);
+        setModalTwoValue((prev) => ({ ...prev, [name]: value }));
     };
+
+    // Handle select change
+    const handleSelectChange = (field, value) => {
+        setModalTwoValue((prev) => ({ ...prev, [field]: value }));
+    };
+
+
 
     const handleOkLowyerTwo = async () => {
 
-
+        setIsModalOpenThree(true)
+        setIsModalOpenTwo(false)
     }
 
 
@@ -154,13 +177,52 @@ const OtpCode = () => {
 
 
 
+    // ===================== three modal start ===================
+    // Handle Availability Selection
+    const handleAvailabilityChange = (value) => setAvailability(value);
+
+    // Handle Time Selection
+    const handleTimeChange = (time, timeString, type) => {
+        if (type === "start") setStartTime(timeString);
+        if (type === "end") setEndTime(timeString);
+    };
+
+
+    const handleOkLowyerThree = async () => {
+        console.log("Uploaded File:", fileList);
+        console.log("Website Link:", webLink);
+        console.log("Availability:", availability);
+        console.log("Start Time:", startTime);
+        console.log("End Time:", endTime);
+    }
+
+    const handleCancelLowyerThree = () => {
+        setIsModalOpenThree(false)
+        setIsModalOpenTwo(true)
+    }
+    // ===================== three modal end  ====================
+
+
+
+    useEffect(() => {
+        // Disable scroll when any modal is open
+        if (isModalOpen || issModalOpenTwo || issModalOpenThree) {
+            document.body.style.overflow = "hidden";
+        } else {
+            // Re-enable scroll when no modal is open
+            document.body.style.overflow = "auto";
+        }
+        return () => {
+            document.body.style.overflow = "auto"; // Cleanup function
+        };
+    }, [isModalOpen,issModalOpenTwo,issModalOpenThree]);
 
 
 
 
 
-
-    console.log(modalOneValue)
+    // console.log(modalOneValue)
+    // console.log(modalTwoValue)
 
     return (
         <AccountCreate>
@@ -324,7 +386,10 @@ const OtpCode = () => {
                     </div>
                     <div className='pb-4'>
                         <p className='text-[14px] font-roboto font-bold text-[#001018]'>Where do you practice</p>
-                        <Input name='practice' placeholder='e.g.: New Jersey, New York, EOIR (Immigration Court)' style={{ width: '100%', height: '40px' }} />
+                        <Input name='practice'
+                            value={modalTwoValue.practice}
+                            onChange={handleInputChange}
+                            placeholder='e.g.: New Jersey, New York, EOIR (Immigration Court)' style={{ width: '100%', height: '40px' }} />
                     </div>
 
 
@@ -334,7 +399,7 @@ const OtpCode = () => {
                             showSearch
                             placeholder="Select..."
                             style={{ width: '100%', height: '40px' }}
-                            onChange={value => handleSelectModalLowyerTwoValue("location", value)}
+                            onChange={(value) => handleSelectChange("location", value)}
                             options={[
                                 { value: 'New Jersey', label: 'New Jersey' },
                                 { value: 'New York', label: 'New York' },
@@ -350,7 +415,7 @@ const OtpCode = () => {
                             showSearch
                             placeholder="Select..."
                             style={{ width: '100%', height: '40px' }}
-                            onChange={value => handleSelectModalLowyerTwoValue("languages", value)}
+                            onChange={(value) => handleSelectChange("languages", value)}
                             options={[
                                 { label: "English", value: "English" },
                                 { label: "Spanish", value: "Spanish" },
@@ -362,20 +427,181 @@ const OtpCode = () => {
 
                     <div className='pb-4'>
                         <p className='text-[14px] font-roboto font-bold text-[#001018]'>Office address</p>
-                        <Input name='address' placeholder='address' style={{ width: '100%', height: '40px' }} />
+                        <Input name='address'
+                            value={modalTwoValue.address}
+                            onChange={handleInputChange}
+                            placeholder='address' style={{ width: '100%', height: '40px' }} />
                     </div>
 
 
                     <div className='flex justify-between gap-2'>
                         <div className='pb-4 w-full'>
                             <p className='text-[14px] font-roboto font-bold text-[#001018]'>State</p>
-                            <Input name='state' style={{ width: '100%', height: '40px' }} />
+                            <Input name='state'
+                                value={modalTwoValue.state}
+                                onChange={handleInputChange}
+                                style={{ width: '100%', height: '40px' }} />
                         </div>
 
                         <div className='pb-4 w-full'>
                             <p className='text-[14px] font-roboto font-bold text-[#001018]'>Zip code</p>
-                            <Input name='zipCode' style={{ width: '100%', height: '40px' }} />
+                            <Input
+                                value={modalTwoValue.zipCode}
+                                onChange={handleInputChange}
+                                name='zipCode' style={{ width: '100%', height: '40px' }} />
                         </div>
+                    </div>
+                </div>
+
+            </Modal>
+
+
+            {/* ======================= modal three start ===================== */}
+            <Modal centered open={issModalOpenThree} onOk={handleOkLowyerThree} onCancel={handleCancelLowyerThree}
+                width={600}
+                footer={
+                    <div className="flex justify-between items-center gap-x-4 pt-[24px]">
+                        <button
+                            className="font-roboto w-[40%] h-[40px] md:w-[161px] md:h-[64px] border border-[#1b69ad] text-[#1b69ad] rounded-[5px] text-[16px] font-bold"
+                            onClick={handleCancelLowyerThree}
+                        >
+                            Back
+                        </button>
+                        <button
+                            className="font-roboto w-[40%] h-[40px] md:w-[161px] md:h-[64px] bg-[#1b69ad] text-white rounded-[5px] text-[16px] font-bold"
+                            onClick={handleOkLowyerThree}
+                        >
+                            Continue
+                        </button>
+                    </div>
+                }
+            >
+
+
+                <div>
+                    <svg
+                        className="mb-4 w-[90%] sm:w-[90%] md:w-[90%] lg:w-[90%]"
+                        width="528" height="40" viewBox="0 0 528 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <circle cx="20" cy="20" r="16" fill="#1B69AD" />
+                        <path d="M14.1665 20.8335L17.4998 24.1668L25.8332 15.8335" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <rect x="36" y="19" width="212" height="2" fill="#1B69AD" />
+                        <circle cx="264" cy="20" r="16" fill="#1B69AD" />
+                        <path d="M258.167 20.8335L261.5 24.1668L269.833 15.8335" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <rect x="280" y="19" width="212" height="2" fill="#1B69AD" />
+                        <circle cx="508" cy="20" r="15" stroke="#1B69AD" strokeWidth="2" />
+                        <circle cx="508" cy="20" r="5" fill="#1B69AD" />
+                    </svg>
+
+
+
+                    <hr />
+
+                    <div className='pt-4'>
+                        <Title level={4} className='text-[#000000] font-roboto text-start pb-[8px]'>
+                            Add your profile photo and availability
+                        </Title>
+                    </div>
+
+
+
+                    <div className="pb-4 w-full">
+                        <p className="text-[14px] font-roboto font-bold text-[#001018]">Upload profile photo</p>
+                        <div className="w-full">
+                            <Upload
+                                fileList={fileList}
+                                onChange={handleChange}
+                                beforeUpload={() => false}
+                                style={{ width: '100%', height: '40px' }}
+                                className="upload-component"
+                            >
+                                {fileList.length >= 1 ? null : (
+                                    <Button
+                                        icon={<UploadOutlined />}
+                                        style={{ width: '100%', height: '40px' }}
+                                    >
+                                        Upload Image
+                                    </Button>
+                                )}
+                            </Upload>
+                        </div>
+                    </div>
+
+
+
+
+                    <div className='pb-4'>
+                        <p className='text-[14px] font-roboto font-bold text-[#001018]'>Website link (optional)</p>
+                        <Input
+                         onChange={(e) => setWebLink(e.target.value)}
+                        placeholder='Include a link to your website here' style={{ width: '100%', height: '40px' }} />
+                    </div>
+
+
+                    <div className='pb-4'>
+                        <div className='flex justify-between items-center gap-6 pb-4'>
+                            <div className='w-full'>
+                                <p className='text-[14px] font-roboto font-bold text-[#001018]'>Availability (optional)</p>
+                                <Space wrap>
+                                    <Select
+                                        defaultValue="Select day.."
+                                        style={{ width: 150 }}
+                                        onChange={handleAvailabilityChange}
+                                        options={[
+                                            { value: 'Monday', label: 'Monday' },
+                                            { value: 'Tuesday', label: 'Tuesday' },
+                                            { value: 'Wednesday', label: 'Wednesday' },
+                                            { value: 'Thursday', label: 'Thursday' },
+                                            { value: 'Friday', label: 'Friday' },
+                                            { value: 'Saturday', label: 'Saturday' },
+                                            { value: 'Sunday', label: 'Sunday' },
+                                        ]}
+                                    />
+                                </Space>
+                            </div>
+
+
+                            <div className='w-full'>
+                                <p className='text-[14px] font-roboto font-bold text-primary text-end'>Start Time</p>
+                                <TimePicker style={{ width: "100%", height: '40px' }}  onChange={(time, timeString) => handleTimeChange(time, timeString, "start")}
+                                 />
+                            </div>
+
+                            <div className='w-full'>
+                                <p className='text-[14px] font-roboto font-bold text-primary text-end'>End Time</p>
+                                <TimePicker style={{ width: "100%", height: '40px' }} onChange={(time, timeString) => handleTimeChange(time, timeString, "end")} />
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className='pb-4'>
+                        <p className='text-[14px] font-roboto font-bold text-[#001018]'>Preview</p>
+                        {/* <Space wrap>
+                            {lowyerOptions.map((option) => (
+                                <Button
+                                    key={option}
+                                    onClick={() => handleSelectLowyer(option)}
+                                    style={{
+                                        borderRadius: 20,
+
+                                        backgroundColor: selectedOptions.includes(option) ? "#1b69ad" : "#FFFFFF",
+                                        color: selectedOptions.includes(option) ? "#FFFFFF" : "#1b69ad",
+                                        border: "1px solid #B6B6BA",
+                                        fontWeight: "bold",
+                                        fontSize: "16px",
+                                        fontFamily: "Roboto",
+                                        padding: "20px"
+                                    }}
+                                >
+                                    {option}
+                                </Button>
+                            ))}
+                        </Space> */}
+                    </div>
+                    <div className='flex flex-col md:flex-row gap-[12px]'>
+                        <p className='font-roboto text-[14px] text-[#121221]'>11:30-12:30 pm</p>
+                        <p className='font-roboto text-[14px] text-[#121221]'>11:30-12:30 pm</p>
+                        <p className='font-roboto text-[14px] text-[#121221]'>11:30-12:30 pm</p>
                     </div>
                 </div>
 
