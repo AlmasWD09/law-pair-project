99041b7e874cbaf3015686935a0044598c27486c
almas hossain <almashossain7384@gmail.com>
2025-03-16 21:35:46 +0600

    user profile data get in server-site ok

--- src/pages/profile/UserProfile.jsx
+++ src/pages/profile/UserProfile.jsx
@@ -2,41 +2,42 @@ import { Link } from "react-router-dom";
 import AccountCreate from "../../layout/AccountCreate"
 import { FaEdit } from "react-icons/fa";
 import { Pagination } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Cookies from "js-cookie";
+
 
 const UserProfile = () => {
     const axiosPublic = useAxiosPublic();
     const [currentPage, setCurrentPage] = useState(1)
     const itemsPerPage = 4
+    const [favoriteData, setFavoriteData] = useState([])
+
+    // token get in cookies
+    const userToken = Cookies.get("userToken");
 
 
-    const alluserData = [
-        {
-            "id": 1,
-            "image": "/attorney1.png",
-            "name": "John Doe",
-            "title": "Software Engineer"
-        },
-        {
-            "id": 2,
-            "image": "/attorney1.png",
-            "name": "Jane Smith",
-            "title": "Product Manager"
-        },
-        {
-            "id": 3,
-            "image": "/attorney1.png",
-            "name": "Alice Johnson",
-            "title": "UI/UX Designer"
-        },
-        {
-            "id": 4,
-            "image": "/attorney1.png",
-            "name": "Michael Brown",
-            "title": "Data Scientist"
-        },
-    ];
+
+    // first modal option get server
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axiosPublic.get(`/user/favorite-list?per_page=10`, {
+                    headers: {
+                        Authorization: `Bearer ${userToken}`,
+                        Accept: "application/json",
+                    },
+                });
+
+                setFavoriteData(response.data.favoriteList.data)
+            } catch (error) {
+                console.error('Failed to load data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
 
 
     const handlePageChange = (page) => {
@@ -46,10 +47,11 @@ const UserProfile = () => {
     // Slice data based on current page
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const paginatedData = alluserData.slice(startIndex, endIndex);
+    const paginatedData = favoriteData.slice(startIndex, endIndex);
 
 
     const handleLogout = async () => {
+        console.log('click-----> Log OUT')
 
         // try {
         //     const response = await axiosPublic.post('/logout')
@@ -59,6 +61,7 @@ const UserProfile = () => {
         //     console.log('logout Failed')
         // }
     }
+
 
     return (
         <div className="bg-gray-100">
@@ -136,16 +139,18 @@ const UserProfile = () => {
                                         paginatedData.map((attorney, index) => {
                                             return (
                                                 <div key={index} className="lg:first-letter lg:h-[378px] p-4 shadow-lg rounded-md ">
-                                                    <img src={attorney.image} alt="attorney" className="w-full rounded-md" />
+                                                    <img src='/attorney1.png' alt="attorney" className="w-full rounded-md" />
+                                                    {/* <img src={attorney.image} alt="attorney" className="w-full rounded-md" /> */}
                                                     <div className="flex justify-between items-center">
-                                                        <h2 className="text-[20px] font-bold font-roboto text-[#001018] pb-2 pt-[16px]">{attorney.name}</h2>
+                                                        <h2 className="text-[20px] font-bold font-roboto text-[#001018] pb-2 pt-[16px]">{attorney.state}</h2>
+
                                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM9.3824 11.0689C9.50441 11.1213 9.61475 11.1975 9.707 11.293L11 12.586L14.293 9.29302C14.3852 9.19751 14.4956 9.12133 14.6176 9.06892C14.7396 9.01651 14.8708 8.98892 15.0036 8.98777C15.1364 8.98662 15.2681 9.01192 15.391 9.0622C15.5138 9.11248 15.6255 9.18673 15.7194 9.28063C15.8133 9.37452 15.8875 9.48617 15.9378 9.60907C15.9881 9.73196 16.0134 9.86364 16.0122 9.99642C16.0111 10.1292 15.9835 10.2604 15.9311 10.3824C15.8787 10.5044 15.8025 10.6148 15.707 10.707L11.707 14.707C11.5195 14.8945 11.2652 14.9998 11 14.9998C10.7348 14.9998 10.4805 14.8945 10.293 14.707L8.293 12.707C8.19749 12.6148 8.1213 12.5044 8.0689 12.3824C8.01649 12.2604 7.9889 12.1292 7.98775 11.9964C7.98659 11.8636 8.0119 11.732 8.06218 11.6091C8.11246 11.4862 8.18671 11.3745 8.2806 11.2806C8.3745 11.1867 8.48615 11.1125 8.60904 11.0622C8.73194 11.0119 8.86362 10.9866 8.9964 10.9878C9.12918 10.9889 9.2604 11.0165 9.3824 11.0689Z" fill="#05C793" />
                                                         </svg>
 
                                                     </div>
 
-                                                    <h3 className="text-[14px] font-roboto text-[#001018]">{attorney.title}</h3>
+                                                    <h3 className="text-[14px] font-roboto text-[#001018]">{attorney.categories[0]}</h3>
                                                 </div>
                                             )
                                         })
@@ -154,7 +159,7 @@ const UserProfile = () => {
                                 <div className="py-8">
                                     <Pagination
                                         current={currentPage}
-                                        total={alluserData.length}
+                                        total={favoriteData.length}
                                         pageSize={itemsPerPage}
                                         onChange={handlePageChange}
                                         align="center" />
