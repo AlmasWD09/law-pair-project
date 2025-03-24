dbff26bb0c35946aac51af1a2843d85e87fdcf8c
almas hossain <almashossain7384@gmail.com>
2025-03-24 13:09:15 +0600

    user profile bug fixed

--- src/pages/profile/UserProfile.jsx
+++ src/pages/profile/UserProfile.jsx
@@ -203,11 +203,12 @@ const UserProfile = () => {
     // ========== user profile update modal end  =================
 
 
-    
+
     if (loading) {
         return <LoadindSpenier />
     }
-    
+
+
     return (
         <div className="bg-gray-100">
             <AccountCreate >
@@ -224,10 +225,10 @@ const UserProfile = () => {
                                         alt="User Avatar"
                                     /> : <img
                                         className="object-cover w-[124px] h-[124px] rounded-full"
-                                        src='/attorney1.png'
+                                        src='/defaultImage.webp'
                                         alt="User Avatar"
                                     />
-                                }
+                                    }
                                     <h1 className=" font-bold font-roboto text-[#001018 pl-4 pt-[12px] pb-[24px]">
                                         {first_name} {last_name}
                                     </h1>
@@ -339,9 +340,13 @@ const UserProfile = () => {
                                             <p>{phone}</p>
                                         </div>
                                         <p className="pt-4 flex justify-between font-roboto text-[16px] text-[#10101E] text-wrap">
-                                            <svg width="100" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                <path fillRule="evenodd" clipRule="evenodd" d="M12 21.668C9.72 21.668 6 12.982 6 9.66797C6 8.07667 6.63214 6.55055 7.75736 5.42533C8.88258 4.30011 10.4087 3.66797 12 3.66797C13.5913 3.66797 15.1174 4.30011 16.2426 5.42533C17.3679 6.55055 18 8.07667 18 9.66797C18 12.982 14.28 21.668 12 21.668ZM12 12.668C12.3824 12.668 12.7611 12.5926 13.1144 12.4463C13.4677 12.3 13.7887 12.0855 14.0591 11.8151C14.3295 11.5447 14.544 11.2236 14.6903 10.8703C14.8367 10.517 14.912 10.1384 14.912 9.75597C14.912 9.37356 14.8367 8.99489 14.6903 8.64159C14.544 8.28829 14.3295 7.96728 14.0591 7.69687C13.7887 7.42647 13.4677 7.21197 13.1144 7.06563C12.7611 6.91929 12.3824 6.84397 12 6.84397C11.2277 6.84397 10.487 7.15077 9.9409 7.69687C9.3948 8.24298 9.088 8.98366 9.088 9.75597C9.088 10.5283 9.3948 11.269 9.9409 11.8151C10.487 12.3612 11.2277 12.668 12 12.668Z" fill="#10101E" />
-                                            </svg>
+                                          {
+                                            userData.address ?   <svg width="100" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M12 21.668C9.72 21.668 6 12.982 6 9.66797C6 8.07667 6.63214 6.55055 7.75736 5.42533C8.88258 4.30011 10.4087 3.66797 12 3.66797C13.5913 3.66797 15.1174 4.30011 16.2426 5.42533C17.3679 6.55055 18 8.07667 18 9.66797C18 12.982 14.28 21.668 12 21.668ZM12 12.668C12.3824 12.668 12.7611 12.5926 13.1144 12.4463C13.4677 12.3 13.7887 12.0855 14.0591 11.8151C14.3295 11.5447 14.544 11.2236 14.6903 10.8703C14.8367 10.517 14.912 10.1384 14.912 9.75597C14.912 9.37356 14.8367 8.99489 14.6903 8.64159C14.544 8.28829 14.3295 7.96728 14.0591 7.69687C13.7887 7.42647 13.4677 7.21197 13.1144 7.06563C12.7611 6.91929 12.3824 6.84397 12 6.84397C11.2277 6.84397 10.487 7.15077 9.9409 7.69687C9.3948 8.24298 9.088 8.98366 9.088 9.75597C9.088 10.5283 9.3948 11.269 9.9409 11.8151C10.487 12.3612 11.2277 12.668 12 12.668Z" fill="#10101E" />
+                                        </svg>
+                                        :
+                                        ''
+                                          }
                                             {address}
                                         </p>
                                     </div>
@@ -370,41 +375,45 @@ const UserProfile = () => {
 
 
                             {/*  right side content   */}
-                            <div className="col-span-2 pt-8 md:pt-0">
-                                <h1 className="font-roboto text-[26px] font-bold text-[#000000] ">Your <span className="text-primary">LawPair</span> Suggested TM attorneys</h1>
+                            {
+                                paginatedData.length > 0 && (
+                                    <div className=" col-span-2 pt-8 md:pt-0">
+                                        <h1 className="font-roboto text-[26px] font-bold text-[#000000] ">Your <span className="text-primary">LawPair</span> Suggested TM attorneys</h1>
 
-                                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 place-items-center gap-6 md:gap-2 lg:gap-6">
-                                    {
-                                        paginatedData.map((attorney, index) => {
-                                            return (
-                                                <div key={index} className="lg:first-letter lg:h-[378px] lg:w-[300px] p-4 shadow-lg rounded-md ">
-                                                    <img src={attorney.avatar}
-                                                        alt="Profile"
-                                                        onError={(e) => e.target.src = '/attorney1.png'}
-                                                        className="rounded-md" />
+                                        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 place-items-center gap-6 md:gap-2 lg:gap-6">
+                                            {
+                                                paginatedData.map((attorney, index) => {
+                                                    return (
+                                                        <div key={index} className="lg:first-letter lg:h-[378px] lg:w-[300px] p-4 shadow-lg rounded-md ">
+                                                            <img src={attorney.avatar}
+                                                                alt="Profile"
+                                                                onError={(e) => e.target.src = '/attorney1.png'}
+                                                                className="rounded-md" />
 
-                                                    <div className="flex justify-between items-center">
-                                                        <h2 className="text-[20px] font-bold font-roboto text-[#001018] pb-2 pt-[16px]">{attorney.state}</h2>
+                                                            <div className="flex justify-between items-center">
+                                                                <h2 className="text-[20px] font-bold font-roboto text-[#001018] pb-2 pt-[16px]">{attorney.state}</h2>
 
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM9.3824 11.0689C9.50441 11.1213 9.61475 11.1975 9.707 11.293L11 12.586L14.293 9.29302C14.3852 9.19751 14.4956 9.12133 14.6176 9.06892C14.7396 9.01651 14.8708 8.98892 15.0036 8.98777C15.1364 8.98662 15.2681 9.01192 15.391 9.0622C15.5138 9.11248 15.6255 9.18673 15.7194 9.28063C15.8133 9.37452 15.8875 9.48617 15.9378 9.60907C15.9881 9.73196 16.0134 9.86364 16.0122 9.99642C16.0111 10.1292 15.9835 10.2604 15.9311 10.3824C15.8787 10.5044 15.8025 10.6148 15.707 10.707L11.707 14.707C11.5195 14.8945 11.2652 14.9998 11 14.9998C10.7348 14.9998 10.4805 14.8945 10.293 14.707L8.293 12.707C8.19749 12.6148 8.1213 12.5044 8.0689 12.3824C8.01649 12.2604 7.9889 12.1292 7.98775 11.9964C7.98659 11.8636 8.0119 11.732 8.06218 11.6091C8.11246 11.4862 8.18671 11.3745 8.2806 11.2806C8.3745 11.1867 8.48615 11.1125 8.60904 11.0622C8.73194 11.0119 8.86362 10.9866 8.9964 10.9878C9.12918 10.9889 9.2604 11.0165 9.3824 11.0689Z" fill="#05C793" />
-                                                        </svg>
-                                                    </div>
-                                                    <h3 className="text-[14px] font-roboto text-[#001018]">{attorney.categories[0]}</h3>
-                                                </div>
-                                            )
-                                        })
-                                    }
-                                </div>
-                                <div className="py-8">
-                                    <Pagination
-                                        current={currentPage}
-                                        total={favoriteData.length}
-                                        pageSize={itemsPerPage}
-                                        onChange={handlePageChange}
-                                        align="center" />
-                                </div>
-                            </div>
+                                                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM9.3824 11.0689C9.50441 11.1213 9.61475 11.1975 9.707 11.293L11 12.586L14.293 9.29302C14.3852 9.19751 14.4956 9.12133 14.6176 9.06892C14.7396 9.01651 14.8708 8.98892 15.0036 8.98777C15.1364 8.98662 15.2681 9.01192 15.391 9.0622C15.5138 9.11248 15.6255 9.18673 15.7194 9.28063C15.8133 9.37452 15.8875 9.48617 15.9378 9.60907C15.9881 9.73196 16.0134 9.86364 16.0122 9.99642C16.0111 10.1292 15.9835 10.2604 15.9311 10.3824C15.8787 10.5044 15.8025 10.6148 15.707 10.707L11.707 14.707C11.5195 14.8945 11.2652 14.9998 11 14.9998C10.7348 14.9998 10.4805 14.8945 10.293 14.707L8.293 12.707C8.19749 12.6148 8.1213 12.5044 8.0689 12.3824C8.01649 12.2604 7.9889 12.1292 7.98775 11.9964C7.98659 11.8636 8.0119 11.732 8.06218 11.6091C8.11246 11.4862 8.18671 11.3745 8.2806 11.2806C8.3745 11.1867 8.48615 11.1125 8.60904 11.0622C8.73194 11.0119 8.86362 10.9866 8.9964 10.9878C9.12918 10.9889 9.2604 11.0165 9.3824 11.0689Z" fill="#05C793" />
+                                                                </svg>
+                                                            </div>
+                                                            <h3 className="text-[14px] font-roboto text-[#001018]">{attorney.categories[0]}</h3>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                        <div className="py-8">
+                                            <Pagination
+                                                current={currentPage}
+                                                total={favoriteData.length}
+                                                pageSize={itemsPerPage}
+                                                onChange={handlePageChange}
+                                                align="center" />
+                                        </div>
+                                    </div>)
+
+                            }
                         </div>
                     </div>
                 </section>
