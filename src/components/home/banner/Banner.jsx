9c71ebdd168cc325cd6cfcc3793630b63419b80a
almas hossain <almashossain7384@gmail.com>
2025-04-12 21:52:12 +0600

    banner video thumbail image changes

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -413,14 +413,17 @@ const Banner = () => {
                   onClick={() => setIsPlaying(true)}
                 >
                   <motion.img
-                    src="/video.jpg"
+                    src="/video25.jpg"
                     alt="Video Thumbnail"
                     className="w-full h-full object-cover rounded-2xl"
                     whileHover={{ scale: 1.05 }}
                     transition={{ type: "spring", stiffness: 200 }}
                   />
                   <div className="absolute inset-0 flex items-center justify-center bg-opacity-50">
-                    <PlayCircle size={60} className="text-primary" />
+                    <PlayCircle
+                      size={100}
+                      className="text-primary opacity-50"
+                    />
                   </div>
                 </div>
               ) : (
